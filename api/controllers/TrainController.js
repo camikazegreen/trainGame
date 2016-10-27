65fb09fe5f2078d758fa7cf7936ece5eb50d70dc
camikazegreen <camikazegreen@gmail.com>
2016-10-26 20:24:15 -0700

    added the controllers

---
+++ api/controllers/TrainController.js
@@ -0,0 +1,11 @@
+/**
+ * TrainController
+ *
+ * @description :: Server-side logic for managing trains
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/TrainLineController.js
@@ -0,0 +1,11 @@
+/**
+ * TrainLineController
+ *
+ * @description :: Server-side logic for managing trainlines
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
